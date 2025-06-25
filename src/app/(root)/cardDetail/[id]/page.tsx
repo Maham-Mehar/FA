154c505358a94ee4d6a6f8c66f7171beb61e684c
Maham-Mehar <mahammehar47@gmail.com>
2025-06-26 00:41:46 +0500

    psh

--- src/app/(root)/cardDetail/[id]/page.tsx
+++ src/app/(root)/cardDetail/[id]/page.tsx
@@ -1,23 +1,26 @@
 // src/app/(root)/cardDetail/[id]/page.tsx
+
+import { Fragment } from "react";
 import StartAdventure from "@/Component/readMore/adventure";
 import About from "@/Component/readMore/about";
 import Attractions from "@/Component/readMore/cards";
 import BestLands from "@/Component/readMore/bestLands";
 import YachtSlider from "@/Component/readMore/slider";
 import ChartersAdventure from "@/Component/readMore/yarchtAdventure";
-import { Fragment } from "react";
 import HeroSection from "@/Component/readMore/hero";
 
-interface CardDetailPageProps {
-  params: { id: string };
+interface PageProps {
+  params: {
+    id: string;
+  };
 }
 
-export default function CardDetail({ params }: CardDetailPageProps) {
+export default function Page({ params }: PageProps) {
   const { id } = params;
 
   return (
     <Fragment>
-      <HeroSection id={params.id} />
+      <HeroSection id={id} />
       <About id={id} />
       <Attractions id={id} />
       <BestLands id={id} />
