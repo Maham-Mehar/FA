18f6ab92ed7c73bd7a5964040f5627528780b098
Maham-Mehar <mahammehar47@gmail.com>
2025-07-14 16:45:26 +0500

    push

--- src/Component/crewed_Charter/hero.tsx
+++ src/Component/crewed_Charter/hero.tsx
@@ -18,6 +18,7 @@ const HeroSection = () => {
             {/* Content on top */}
             <div className="flex items-center justify-center h-full text-center text-white px-4">
                 <div className="flex flex-col justify-center items-center max-w-4xl mx-auto">
+                    {/* Title Text */}
                     <p className="text-[28px] md:text-[36px] lg:text-[36px] xl:text-[40px] font-bold font-playfair mb-4 leading-tight md:max-w-2xl lg:max-w-2xl w-full">
                         Yacht Charter Phuket      </p>
                     <img src={PngIcons.rframe2} alt="" className="w-[500px] flex justify-center text-center mx-auto" />
