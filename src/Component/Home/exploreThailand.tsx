b9b36f3ea1794742c73d55444a9a4d85c7d33941
Maham-Mehar <mahammehar47@gmail.com>
2025-07-23 15:47:07 +0500

    push

--- src/Component/Home/exploreThailand.tsx
+++ src/Component/Home/exploreThailand.tsx
@@ -8,6 +8,9 @@ const ExploreThailand = () => {
                 heading="Explore Thailand with a private Yacht"
                 description="Explore Thailand’s remote islands with a private yacht, enjoy exclusive dining onboard and unwind with sunset yoga on white sandy beaches. Swim in sparkling waters and create lasting memories. Book your private yacht in Phuket." />
             <Cards />
+            <div className="flex justify-center items-center">
+            <button className="bg-zink font-poppins text-white font-semibold text-base px-5 py-3 rounded-lg hover:bg-[#D6AB61]">View More Destinations</button>
+            </div>
             <SetSail bgImage="/images/homeimg6.png" />
         </div>
     )
