2fcf2f0d3432fba41de7ab5e679f7cb3fa31cf53
Maham-Mehar <mahammehar47@gmail.com>
2025-07-28 18:25:14 +0500

    push

--- src/Component/Charter/yachtAdventure.tsx
+++ src/Component/Charter/yachtAdventure.tsx
@@ -1,9 +1,12 @@
 import SetSail from "@/common/yarchtcharter"
 import PngIcons from "@/icons/pngIcon"
-const YachtAdventure =()=>{
-    return(
+const YachtAdventure = () => {
+    return (
         // Reuse the component from the Common
-        <SetSail bgImage={PngIcons.crewedimg2} />
+        <SetSail bgImage={PngIcons.crewedimg2}
+            bgColor="#034250"
+            overlayOpacity={0.3}
+        />
     )
 }
 export default YachtAdventure
