ccec5d6ee67c894fd811f678b481d68c85a19db9
Maham-Mehar <mahammehar47@gmail.com>
2025-07-29 12:51:32 +0500

    push

--- src/Component/cabinCharter/moments2Remember.tsx
+++ src/Component/cabinCharter/moments2Remember.tsx
@@ -1,5 +1,4 @@
 "use client";
-import HeadingContent from "@/common/heading";
 import Image from "next/image";
 import React from "react";
 
@@ -7,12 +6,23 @@ const Moments2Remember: React.FC = () => {
   return (
     <section className="bg-white py-4 md:py-9 lg:py-12 xl:py-16 px-4 xl:px-0">
       {/* Heading */}
-      <div className="text-center mb-[40px]">
-        <HeadingContent
-          heading="Sailing Moments to Remember"
-          description="A collection of memories from guests who turned dreams into voyages"
+      <div className="text-center mb-[40px] flex flex-col items-center">
+        <p className="text-[28px] md:text-[32px] lg:text-[36px] xl:text-[40px] font-playfair text-zink text-green font-extrabold">
+          Sailing Moments to Remember
+        </p>
+
+        {/* Gold underline decoration */}
+        <img
+          src="/images/rframe.png"
+          alt="Divider"
+          className="w-[500px] flex justify-center text-center my-3 mx-auto"
         />
+
+        <p className="text-base md:text-lg lg:text-xl text-zink font-sourceSansPro text-center max-w-[420px]">
+          A collection of memories from guests who turned dreams into voyages
+        </p>
       </div>
+
 
       {/* Collage */}
       <div className="max-w-7xl mx-auto flex flex-col gap-2 md:gap-2 lg:gap-3 xl:gap-4 items-center mt-8">
@@ -30,7 +40,7 @@ const Moments2Remember: React.FC = () => {
           <div className="">
             <img src="/images/img6.png" alt="" className="rounded-xl" />
           </div>
-          <div className="-mt-[55px] md:-mt-[65px] lg:-mt-[75px] xl:-mt-[82px] hidden md:block">
+          <div className="-mt-[55px] md:-mt-[65px] lg:-mt-[75px] xl:-mt-[90px] hidden md:block">
             <img src="/images/img8.png" alt="" className="rounded-xl" />
           </div>
           <div className="mt-5 md:mt-7 lg:mt-10 xl:mt-13">
