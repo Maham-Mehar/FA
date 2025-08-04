64406119cf83f604a90d0c3759ac96b272fd14ff
Maham-Mehar <mahammehar47@gmail.com>
2025-08-04 15:10:31 +0500

    push

--- src/app/(root)/crewed_boats/[slug]/page.tsx
+++ src/app/(root)/crewed_boats/[slug]/page.tsx
@@ -1,17 +1,9 @@
-import HeroSection from '@/Component/YachtDetail/hero';
+import HeroSection from "@/Component/YachtDetail/hero";
 
-interface PageProps {
-  params: {
-    slug: string;
-  };
-}
-
-const YachtDetail= ({ params }: PageProps) => {
+export default function YachtDetailPage({ params }: { params: { slug: string } }) {
   return (
     <div>
       <HeroSection slug={params.slug} />
     </div>
   );
-};
-
-export default YachtDetail;
+}
